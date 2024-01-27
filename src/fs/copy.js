f62b73f50d469252a526d77abd61062c0e00ac07
Artemy Dyachuk <artem002498@gmail.com>
2024-01-27 17:57:42 +0600

    add: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const copy = async () => {
-    // Write your code here 
+  const sourceDir = path.resolve("src/fs/files");
+  const targetDir = path.resolve("src/fs/files_copy");
+
+  try {
+    await fs.cp(sourceDir, targetDir, {
+      force: false,
+      recursive: true,
+      errorOnExist: true,
+    });
+  } catch (err) {
+    if (err.code === "ENOENT" || err.code === "ERR_FS_CP_EEXIST") {
+      throw new Error("FS operation failed");
+    } else {
+      throw err;
+    }
+  }
 };
 
 await copy();
