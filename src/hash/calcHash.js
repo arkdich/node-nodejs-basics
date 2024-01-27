670e78414de4b5e1024bb1aef9a85d2713a57bee
Artemy Dyachuk <artem002498@gmail.com>
2024-01-28 02:29:17 +0600

    add: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = path.resolve("src/hash/files/fileToCalculateHashFor.txt");
+
+  const hash = createHash("sha256");
+
+  const readStream = createReadStream(fileName, { encoding: "utf-8" });
+
+  readStream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  readStream.on("end", () => {
+    process.stdout.write(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
