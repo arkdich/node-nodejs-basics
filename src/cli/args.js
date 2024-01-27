5ddaef9179b1dc63aa0085d21e50539465a85501
Artemy Dyachuk <artem002498@gmail.com>
2024-01-27 21:05:45 +0600

    add: implement cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  const ARG_PREFIX = "--";
+
+  const parsedArgs = [];
+
+  args.forEach((arg, index) => {
+    if (!arg.startsWith(ARG_PREFIX)) return;
+
+    parsedArgs.push({
+      key: arg.substring(2),
+      value: args[index + 1],
+    });
+  });
+
+  parsedArgs.forEach(({ key, value }) => {
+    console.log(`${key} is ${value}`);
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const args = process.argv;
+  const ARG_PREFIX = "RSS_";
+
+  const filteredArgs = args.filter((arg) => arg.startsWith(ARG_PREFIX));
+
+  console.log(filteredArgs.join("; "));
 };
 
 parseEnv();
