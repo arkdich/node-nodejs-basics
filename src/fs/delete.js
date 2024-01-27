a532c0ea6e17087c013391d8df702ad54afed0ce
Artemy Dyachuk <artem002498@gmail.com>
2024-01-27 20:12:25 +0600

    add: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const remove = async () => {
-    // Write your code here 
+  const fileName = path.resolve("src/fs/files/fileToRemove.txt");
+
+  try {
+    await fs.rm(fileName, { force: false });
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      throw err;
+    }
+  }
 };
 
 await remove();
