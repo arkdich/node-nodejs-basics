4b8cc2530ff21a911e979425652c9b9d1a315dd6
Artemy Dyachuk <artem002498@gmail.com>
2024-01-28 00:28:08 +0600

    add: implement streams read/write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = path.resolve("src/streams/files/fileToRead.txt");
+
+  const readStream = fs.createReadStream(fileToRead);
+
+  readStream.pipe(process.stdout);
 };
 
 await read();
