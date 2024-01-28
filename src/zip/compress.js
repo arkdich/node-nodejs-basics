fffd2ada4982cabeb3a2dfbdc170f5395e2697a1
Artemy Dyachuk <artem002498@gmail.com>
2024-01-28 13:34:53 +0600

    add: implement zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import fs from "fs/promises";
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = path.resolve("src/zip/files/fileToCompress.txt");
+  const destination = path.resolve("src/zip/files/archive.gz");
+
+  await fs.access(fileToCompress, fs.constants.R_OK);
+
+  const readStream = createReadStream(fileToCompress, {
+    encoding: "utf-8",
+  });
+  const writeStream = createWriteStream(destination, { encoding: "utf-8" });
+  const gzipStream = createGzip();
+
+  await pipeline(readStream, gzipStream, writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from "fs";
+import fs from "fs/promises";
+import path from "path";
+import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const fileToDecompress = path.resolve("src/zip/files/archive.gz");
+  const destination = path.resolve("src/zip/files/fileToCompress.txt");
+
+  await fs.access(fileToDecompress, fs.constants.R_OK);
+
+  const readStream = createReadStream(fileToDecompress);
+  const writeStream = createWriteStream(destination, { encoding: "utf-8" });
+  const gunzipStream = createGunzip();
+
+  await pipeline(readStream, gunzipStream, writeStream);
 };
 
 await decompress();
