39cb4f92d16e1c0bc617e63561decda3906c6445
Artemy Dyachuk <artem002498@gmail.com>
2024-01-28 23:02:10 +0600

    add: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { fork, spawn } from 'node:child_process'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+  const filePath = path.resolve('src/cp/files/script.js')
+
+  try {
+    await fs.access(filePath, fs.constants.X_OK)
+  } catch (err) {
+    if (err.code === 'EACCES') {
+      await fs.chmod(filePath, 511)
+    } else {
+      throw err
+    }
+  }
+
+  const cp = fork(filePath, args)
+}
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2'])
