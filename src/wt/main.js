a11a99cee7d3edcc42b89c31922898eaccd8208e
Artemy Dyachuk <artem002498@gmail.com>
2024-01-28 23:53:16 +0600

    add: implement workers module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import os from 'os'
+import path from 'path'
+import { Worker } from 'worker_threads'
 
-await performCalculations();
+const performCalculations = async () => {
+  const workerScript = path.resolve('src/wt/worker.js')
+
+  const availableCpus = os.cpus()
+  const workerResults = Array(availableCpus.length)
+
+  let dataToProcess = 10
+  let completedCount = 0
+
+  availableCpus.forEach((_, index) => {
+    const worker = new Worker(workerScript)
+
+    worker.on('message', ({ index, response }) => {
+      workerResults[index] = response
+
+      if (++completedCount === availableCpus.length) {
+        console.log(workerResults)
+      }
+    })
+
+    worker.postMessage({
+      index,
+      data: dataToProcess++,
+    })
+  })
+}
+
+await performCalculations()
