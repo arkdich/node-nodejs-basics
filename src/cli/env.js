a596b05a93167205c5ed1c93af0255aa8492b44a
Artemy Dyachuk <diachuk.a@mybuh.kz>
2024-01-30 13:52:12 +0600

    fix: env.js proccess.env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,8 @@
 const parseEnv = () => {
-  const args = process.argv
+  const args = Object.entries(process.env)
   const ARG_PREFIX = 'RSS_'
 
-  const filteredArgs = args.filter((arg) => arg.startsWith(ARG_PREFIX))
+  const filteredArgs = args.filter(([key]) => key.startsWith(ARG_PREFIX))
 
   console.log(filteredArgs.join('; '))
 }
